c5159cc6aee6f1f3b2149369fa89bbe6c3ef1693
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-12 20:57:51 -0300

    Logic in result component defined

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { SearchService } from "src/app/components/search/search.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-result',
-  templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  selector: "app-result",
+  templateUrl: "./result.component.html",
+  styleUrls: ["./result.component.scss"],
 })
-export class ResultComponent implements OnInit {
+export class ResultComponent implements OnInit, OnDestroy {
+  userName: string;
+  user: any;
+  private sub: any;
+  private subUser: any;
 
-  constructor() { }
+  constructor(
+    private searchService: SearchService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.sub = this.route.params.subscribe((params) => {
+      this.userName = params["query"];
+      this.searchService.searchUser(this.userName);
+    });
+    this.subUser = this.searchService.getUser().subscribe((user) => {
+      this.user = user;
+    });
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+    this.subUser.unsubscribe();
+  }
 }
