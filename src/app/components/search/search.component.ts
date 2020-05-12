a7e45ec33d4407704aae6d28efdc25f0039ead23
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-12 18:05:30 -0300

    Logic of search component defined

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  @Input() userName: string;
+  form: any;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      userName: [(this.userName ? this.userName : '')]
+    })
   }
 
+  onSubmit(): void {
+    this.router.navigate(['/result', this.form.value.userName])
+  }
 }
