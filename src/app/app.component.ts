9c3e0b4d3bebc4b755e06d08656089eb4166b6a6
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-13 17:38:02 -0300

    Some updates in home and search components styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  /* se o atributo for template, definimos o html aqui no formato inline, se for templateUrl, 
-  aponta-se o caminho para o arquivo */
-  templateUrl: './app.component.html',
-  styles: []
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styles: [],
 })
 export class AppComponent {}
